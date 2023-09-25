469d3d9867d8822fbb82bdfca9357a7aa07da341
prtrvd <preeti.trivedi@nagarro.com>
2023-09-25 16:51:30 +0530

    Third Commit

--- FirstCode.java
+++ FirstCode.java
@@ -1,7 +1,7 @@
 public class FirstCode {
 
     public static void main(String[] args) {
-        int age=20;
+        int age=22;
         if(age>18){
             System.out.println("Age is greater than 18");
         }
--- SecondCode.java
+++ SecondCode.java
@@ -3,7 +3,7 @@ public class SecondCode {
     public static void main(String[] args) {
         int number=13;
         if(number%2==0){
-            System.out.println("even number");
+            System.out.println("Even number");
         }else{
             System.out.println("Odd number");
         }
